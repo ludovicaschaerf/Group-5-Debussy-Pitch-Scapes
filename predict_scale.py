39ab68ac21378101b1db61debc2e16257ecb9c3b
ludovicaschaerf <ludovica.schaerf@gmail.com>
2021-05-13 12:14:29 +0200

    improved prediction

--- predict_scale.py
+++ predict_scale.py
@@ -15,7 +15,7 @@ orig_coeffs = [[1,1,1,1,1,1], [0,1,0,1,0,1], [0.17, 0.33, 0.75, 0.58, 0.64, 0.33
                [0, 0.64, 0,0,0, 0.33], [0,0,0.71,0,0,0],
                [0,0,0,0.5,0,0], [0,0,0,0,0,0]]
 
-def predict_scale(score, hor_ratio=(0,1), ver_ratio=(0,1)):
+def predict_scale(score, hor_ratio=(0,1), ver_ratio=1):
     ''' Function that takes the filepath of a MIDI file, the height range (how high up the triangle)
         and the horizontal range (how far into the piece), where 0 is the beginnning and 1 is the end, 
         and finds the most fitting scale/chord for that range of that piece based on how similar the 
@@ -28,9 +28,8 @@ def predict_scale(score, hor_ratio=(0,1), ver_ratio=(0,1)):
     for i in range(utm.shape[0]):
         utm[i,:][utm[i,:] == 0] = np.mean(utm[i,:][utm[i,:] != 0])
     sel = utm[int(utm.shape[0] * hor_ratio[0]):
-              int(utm.shape[0] * hor_ratio[1])-1][int(utm.shape[1] * ver_ratio[0]):
-                                                  int(utm.shape[1] * ver_ratio[1])-1]
-    coeffs = np.mean(np.mean(sel[1:], axis=1), axis=0)[1:] / np.max(np.abs(sel))
+              int(utm.shape[0] * hor_ratio[1])-1][int(utm.shape[1] * ver_ratio)-1]
+    coeffs = np.mean(sel[1:], axis=0)[1:] / np.max(np.abs(sel))
     sim = []
     print('the most resonant coefficient is: ', np.argmax(coeffs) + 1)
     for scale in orig_coeffs:
@@ -39,4 +38,4 @@ def predict_scale(score, hor_ratio=(0,1), ver_ratio=(0,1)):
         sim_ = np.abs(coeffs - np.array(scale)/norm2)
         sim.append(np.sum(sim_))
     print(score, 'is a', dic[np.argmin(sim[1:15]) + 1])
-    return np.argmax(coeffs) + 1, dic[np.argmin(sim[1:15]) + 1]
+    return np.argmax(coeffs) + 1, dic[np.argmin(sim[1:15]) + 1], coeffs
